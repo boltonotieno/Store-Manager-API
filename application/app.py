f557d667170787edf6b94f559d99336487195d53
Bolton <bolton.otieno@gmail.com>
2018-10-16 09:35:45 +0300

    [Chore #161219589] remove BaseTest

--- application/app.py
+++ application/app.py
@@ -2,16 +2,16 @@ from flask import Flask, Blueprint
 from flask_restplus import Api
 from instance.config import app_config
 
-blue=Blueprint('api', __name__)
-api=Api(blue)
+blue = Blueprint('api', __name__)
+api = Api(blue)
 
-# Create the applicatiion
+# Create our application
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-     
+    
     # register Blueprint
-    app.register_blueprint(blue,url_prefix='/api/v1')
+    app.register_blueprint(blue, url_prefix='/api/v1')
 
     return app
