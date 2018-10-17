980e3126141bf1f57932686b1a010afaa20759bf
Bolton <bolton.otieno@gmail.com>
2018-10-17 18:52:50 +0300

    [Finishes #161289172] create sales model

---
+++ application/api/v1/models/sales_model.py
@@ -0,0 +1,17 @@
+class Sales():
+    """Class contain sales model functions"""
+
+    def __init__(self):
+        """Initialises the sales model"""
+
+    def create_sale(self):
+        """Creates a new sale"""
+        pass
+
+    def get_all_sale(self):
+        """Fetches all sales"""
+        pass
+
+    def get_one_sale(self, sale_id):
+        """Fetches one sale by id"""
+        pass
