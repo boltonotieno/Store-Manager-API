5e4de8415aa779620366e194ad17bf27583c64ca
Bolton <bolton.otieno@gmail.com>
2018-10-17 09:42:13 +0300

    [Feature #161276276] add model

--- application/api/v1/models/product_model.py
+++ application/api/v1/models/product_model.py
@@ -24,3 +24,9 @@ class Product(object):
     def get_all_product(self):
         """Fetches all products"""
         return {'Products' : self.products, 'message' : 'success' }
+
+    def get_one_product(self, product_id):
+        if product_id in self.products:
+            return {'Product' : self.products[product_id], 'message' : 'success'}
+        return {'message' : 'Not found'}
+
