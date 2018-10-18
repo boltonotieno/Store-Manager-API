04b5a8dc5eaa5b2206d8c1c8d1710fd1a5374a8e
Bolton <bolton.otieno@gmail.com>
2018-10-19 02:50:59 +0300

    [Chore #161331388]add login tests

--- application/api/tests/v1/test-registration_login.py
+++ application/api/tests/v1/test-registration_login.py
@@ -16,7 +16,7 @@ class TestAuthentication(unittest.TestCase):
             'name' : 'Jane Doe',
             'username' : 'jdoe',
             'email' : 'jdoe@gmail.com',
-            'password' : 'password',
+            'password' : 'jdoepass',
             'gender' : 'female',
             'role': 'admin'
         }
@@ -24,10 +24,15 @@ class TestAuthentication(unittest.TestCase):
             'name' : 'Jane Doe',
             'username' : 'jdoe',
             'email' : 'jdoe@gmail.com',
-            'password' : 'password',
+            'password' : 'jdoepass',
             'gender' : 'female',
             'role': 'admin'
         }
+        self.data_3 = {
+            'username' : 'jdoe',
+            'password' : 'jdoepass'
+        }
+        
 
     def test_registration(self):
         """Test registeration of new users"""
@@ -40,7 +45,7 @@ class TestAuthentication(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
 
     def test_registration_of_existing_users(self):
-        """Test registeration of existing users"""
+        """Test registration of existing users"""
         response = self.client.post('/api/v1/auth/registration', 
         data= json.dumps(self.data_2),
         content_type='application/json')
@@ -49,17 +54,61 @@ class TestAuthentication(unittest.TestCase):
         self.assertEqual(result['message'], 'User jdoe already exist')
 
     def test_empty_fields(self):
-        """Test registeration of with an empty field"""
+        """Test registeration of with missing username"""
         response = self.client.post('/api/v1/auth/registration', 
         data= json.dumps({
             'name' : 'Jane Doe',
             'email' : 'jdoe@gmail.com',
-            'password' : 'password',
+            'password' : 'jdoepass',
             'gender' : 'female',
             'role': 'admin'}),
         content_type='application/json')
 
-        result = json.loads(response.data.decode())
+        result = json.loads(response.data)
         self.assertEqual(result['message'], {"username": "This field cannot be blank"})
+        self.assertEqual(response.status_code, 400)
 
-    
+    def test_login(self):
+        """Test login of users"""
+        response = self.client.post('/api/v1/auth/login', 
+        data= json.dumps(self.data_2),
+        content_type='application/json')
+
+        result = json.loads(response.data)
+        self.assertEquals(result['message'], 'Logged in as jdoe ')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_with_invalid_username(self):
+        """Test login with invalid username"""
+        response = self.client.post('/api/v1/auth/login', 
+        data= json.dumps({
+            'username' : 'jdoees',
+            'email' : 'jdoepass'}),
+        content_type='application/json')
+
+        result = json.load(response.data)
+        self.assertEqual(result['message'], 'User jdoees does not exist')
+
+    def test_login_with_wrong_passoword(self):
+        """Test login with wrong password"""
+        response = self.client.post('/api/v1/auth/login', 
+        data= json.dumps({
+            'username' : 'jdoe',
+            'email' : 'jdoepassword'}),
+        content_type='application/json')
+
+        result = json.load(response.data)
+        self.assertEqual(result['message'], 'Wrong credentials')
+
+
+    def test_login_with_missing_field(self):
+        """Test login with missing password"""
+        response = self.client.post('/api/v1/auth/login',
+        data= json.dumps({'username' : 'jdoe'}),
+        content_type='application/json')
+
+        result = json.loads(response.data)
+        self.assertEquals(result['message'], {"password": "This field cannot be blank"})
+
+
+
