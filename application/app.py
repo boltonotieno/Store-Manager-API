080e4ef46b65b634e0b07d266ee50d7af1073aa8
Bolton <bolton.otieno@gmail.com>
2018-10-18 15:55:42 +0300

    [Chore #161312234] update gitignore

--- application/app.py
+++ application/app.py
@@ -7,7 +7,7 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-     
+
     # register Blueprint
     from .api.v1 import version1 as v1
     app.register_blueprint(v1)
