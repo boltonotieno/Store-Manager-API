3cd36be84f9bf965e26a44bf6ce2703dff9e9a95
Bolton <bolton.otieno@gmail.com>
2018-10-19 03:30:47 +0300

    [Chore #161331388]add user model

---
+++ application/api/v1/models/user_model.py
@@ -0,0 +1,17 @@
+class Users():
+    """Class contain user model functions"""
+    def __init__(self):
+        """Initialises the sales model"""
+        pass
+
+    def create_user(self, name, price, quantity):
+        """Creates a new sale"""
+        pass
+    
+    def get_all_users(self):
+        """Fetches all users"""
+        pass
+
+    def get_one_sale(self, user_id):
+        """Fetches one user by id"""
+        pass
