a40590de81636ee8b8652c9f3190af114fc7a4f2
Bolton <bolton.otieno@gmail.com>
2018-10-20 09:03:53 +0300

    [Bug #161332963] remove claim loader

--- application/app.py
+++ application/app.py
@@ -12,19 +12,6 @@ def create_app(config_name):
     app.config['JWT_SECRET_KEY'] = 'secret'
     jwt=JWTManager(app)
 
-    @jwt.user_claims_loader
-    def add_claims_to_access_token(user_object):
-        '''add role claims to access token'''
-        return {'role': user_object['role']}
-
-    @jwt.user_identity_loader
-    def user_identity_lookup(user_object):
-        '''set token identity from user_object passed to username'''
-        print(user_object)
-        return user_object["username"]
-
-    
-
     # register Blueprint
     from .api.v1 import version1 as v1
     app.register_blueprint(v1)
