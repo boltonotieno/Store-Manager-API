796bcdbd215ce65619d4c9ff8aa6b4068702f140
Bolton <bolton.otieno@gmail.com>
2018-10-23 19:27:11 +0300

    [Chore #161421440] add PUT and DELETE category test

--- app/api/tests/v2/test_category.py
+++ app/api/tests/v2/test_category.py
@@ -111,6 +111,74 @@ class TestCategory(unittest.TestCase):
         self.assertEqual(result_get_one['message'], 'success')
         self.assertEqual(response.status_code, 200)
 
+    def test_modify_category(self):
+        """Test if API can modify(PUT) a single category"""
+        
+        #register user
+        response_reg = self.client.post('/api/v2/auth/signup', 
+        data= json.dumps(self.data_reg),
+        content_type='application/json')
+
+        #user login
+        response_login = self.client.post('/api/v2/auth/login', 
+        data= json.dumps(self.data_login),
+        content_type='application/json')
+        result_login = json.loads(response_login.data)
+        token = result_login['access_token']
+
+        #user post category
+        response = self.client.post('/api/v2/category',
+        headers = dict(Authorization='Bearer '+token),
+        data= json.dumps(self.data),
+        content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        #user modify category
+        response_modify = self.client.put('/api/v2/category/1',
+        headers = dict(Authorization='Bearer '+token),
+        data= json.dumps({'name' : 'spirit'}),
+        content_type='application/json')
+        result_modify_one = json.loads(response_modify.data)
+        self.assertEqual(result_modify_one['message'], 'successfuly modified ')
+        self.assertEqual(response_modify.status_code, 200)
+
+    def test_delete_category(self):
+        """Test if API can DELETE  a single category"""
+
+        #register user
+        response_reg = self.client.post('/api/v2/auth/signup', 
+        data= json.dumps(self.data_reg),
+        content_type='application/json')
+
+        #user login
+        response_login = self.client.post('/api/v2/auth/login', 
+        data= json.dumps(self.data_login),
+        content_type='application/json')
+        result_login = json.loads(response_login.data)
+        token = result_login['access_token']
+
+        #user post category
+        response = self.client.post('/api/v2/category',
+        headers = dict(Authorization='Bearer '+token),
+        data= json.dumps(self.data),
+        content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        #user delete category
+        response_delete = self.client.delete('/api/v2/category/1',
+        headers = dict(Authorization='Bearer '+token))      
+        result_delete_one = json.loads(response_modify.data)
+        self.assertEqual(result_modify_one['message'], 'successfuly deleted')
+        self.assertEqual(response_modify.status_code, 200)
+
+        #Test if the category has been actually deleted by trying to GET it
+        response_get = self.client.get('/api/v2/category/1',
+        headers = dict(Authorization='Bearer '+token))
+        result_get = json.loads(response_get.data)
+        self.assertEqual(result_get['message'], 'category not found'])
+        self.assertEqual(response.status_code, 404)
+
+
     def tearDown(self):
         """Removes all initialised variables"""
         self.app_context.pop()
