d6979598a0679aef2bbdb8d0797a6810f2068fea
Bolton <bolton.otieno@gmail.com>
2018-10-23 13:56:27 +0300

    [Chore  #161409335] create database connection

--- instance/config.py
+++ instance/config.py
@@ -5,6 +5,7 @@ import os
 class Config(object):
     """Main  configuration class."""
     DEBUG = False
+    DATABASE_URL = os.getenv('DATABASE_URL')
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
@@ -15,6 +16,7 @@ class TestingConfig(Config):
     """Configurations for Testing."""
     TESTING = True
     DEBUG = True
+    DATABASE_URL="dbname='test_store_manager' host='localhost' port='5432' user='bolt' password='root123!'"
 
 app_config = {
     'development': DevelopmentConfig,
