a5805c1bafd04ce979be29e006df0911ca3cda80
Bolton <bolton.otieno@gmail.com>
2018-10-23 14:46:24 +0300

    [Chore  #161412413] create setup

---
+++ app/api/tests/v2/test_registration.py
@@ -0,0 +1,28 @@
+import unittest
+import os
+import json
+from app import create_app
+
+class TestAuthentication(unittest.TestCase):
+    """Authentication TestCases Class"""
+
+    def setUp(self):
+        """ Define tests variables"""
+        self.app = create_app(config_name='testing')
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.data = {
+            'name' : 'Jane Doe',
+            'username' : 'jdoe',
+            'email' : 'jdoe@gmail.com',
+            'password' : 'jdoepass',
+            'gender' : 'female',
+            'role': 'admin'
+        }
+  
+
+    def tearDown(self):
+        """Removes all initialised variables"""
+        self.app_context.pop()
+
