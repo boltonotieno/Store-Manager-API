f08e1d08c042f3e06fc5838c4ef763f99b60cf37
Bolton <bolton.otieno@gmail.com>
2018-10-23 16:24:08 +0300

    [Chore  #161415020] set up test file

---
+++ app/api/tests/v2/test_product.py
@@ -0,0 +1,40 @@
+import unittest
+import os
+import json
+from app import create_app
+
+class TestProducts(unittest.TestCase):
+    """Product TestCases Class"""
+
+    def setUp(self):
+        """ Define test variables"""
+        self.app = create_app(config_name='testing')
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.data = {
+            'name' : 'Pilsner',
+            'price' : '200',
+            'quantity' : '20',
+            'min_quantity' : '5',
+            'category' : 'beer'
+        }
+        self.data_reg = {
+            'name' : 'Jane Doe',
+            'username' : 'jdoe',
+            'email' : 'jdoe@gmail.com',
+            'password' : 'jdoepass',
+            'gender' : 'female',
+            'role': 'admin'
+        }
+        self.data_login = {
+            'username' : 'jdoe',
+            'password' : 'jdoepass'
+        }
+
+    def tearDown(self):
+        """Removes all initialised variables"""
+        self.app_context.pop()
+
+if __name__=='__main__':
+    unittest.main()
