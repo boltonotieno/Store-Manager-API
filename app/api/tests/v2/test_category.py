561196e476a4b6f8b95e24bf83ed6ec4c742f50a
Bolton <bolton.otieno@gmail.com>
2018-10-23 19:25:31 +0300

    [Chore #161421440] add POST and GET category test

--- app/api/tests/v2/test_category.py
+++ app/api/tests/v2/test_category.py
@@ -28,6 +28,88 @@ class TestCategory(unittest.TestCase):
             'password' : 'jdoepass'
         }
 
+    def test_post_category(self):
+        """Test if API can POST new product category"""
+
+        #register user
+        response_reg = self.client.post('/api/v2/auth/signup', 
+        data= json.dumps(self.data_reg),
+        content_type='application/json')
+
+        #user login
+        response_login = self.client.post('/api/v2/auth/login', 
+        data= json.dumps(self.data_login),
+        content_type='application/json')
+        result_login = json.loads(response_login.data)
+        token = result_login['access_token']
+
+        #user post category
+        response = self.client.post('/api/v2/category', 
+        headers = dict(Authorization='Bearer '+token),
+        data= json.dumps(self.data),
+        content_type='application/json')
+        
+        result = json.loads(response.data)
+        self.assertEqual(result['message'], 'Created successfully' )
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_all_category(self):
+        """Test if API can GET all category"""
+
+         #register user
+        response_reg = self.client.post('/api/v2/auth/signup', 
+        data= json.dumps(self.data_reg),
+        content_type='application/json')
+
+        #user login
+        response_login = self.client.post('/api/v2/auth/login', 
+        data= json.dumps(self.data_login),
+        content_type='application/json')
+        result_login = json.loads(response_login.data)
+        token = result_login['access_token']
+
+        #user post category
+        response = self.client.post('/api/v2/category',
+        headers = dict(Authorization='Bearer '+token),
+        data= json.dumps(self.data),
+        content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        
+        #user get all category
+        response= self.client.get('/api/v2/category',
+        headers = dict(Authorization='Bearer '+token))
+        result_get = json.loads(response.data)
+        self.assertEqual(result_get['message'], 'success')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_category_by_id(self):
+        """Test if API can GET single category by id"""
+
+         #register user
+        response_reg = self.client.post('/api/v2/auth/signup', 
+        data= json.dumps(self.data_reg),
+        content_type='application/json')
+
+        #user login
+        response_login = self.client.post('/api/v2/auth/login', 
+        data= json.dumps(self.data_login),
+        content_type='application/json')
+        result_login = json.loads(response_login.data)
+        token = result_login['access_token']
+
+        #user post category
+        response = self.client.post('/api/v2/category',
+        headers = dict(Authorization='Bearer '+token),
+        data= json.dumps(self.data),
+        content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        #user get one category
+        response = self.client.get('/api/v2/category/1',
+        headers = dict(Authorization='Bearer '+token))
+        result_get_one = json.loads(response.data)
+        self.assertEqual(result_get_one['message'], 'success')
+        self.assertEqual(response.status_code, 200)
 
     def tearDown(self):
         """Removes all initialised variables"""
