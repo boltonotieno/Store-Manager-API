52665739c813d54ad19502c75631f21e18841bcf
Bolton <bolton.otieno@gmail.com>
2018-10-23 16:27:15 +0300

    [Chore  #161415020] add POST and GET  product tests

--- app/api/tests/v2/test_product.py
+++ app/api/tests/v2/test_product.py
@@ -32,6 +32,89 @@ class TestProducts(unittest.TestCase):
             'password' : 'jdoepass'
         }
 
+    def test_post_product(self):
+        """Test if API can POST new products"""
+
+        #register user
+        response_reg = self.client.post('/api/v2/auth/signup', 
+        data= json.dumps(self.data_reg),
+        content_type='application/json')
+
+        #user login
+        response_login = self.client.post('/api/v2/auth/login', 
+        data= json.dumps(self.data_login),
+        content_type='application/json')
+        result_login = json.loads(response_login.data)
+        token = result_login['access_token']
+
+        #user post product
+        response = self.client.post('/api/v2/product', 
+        headers = dict(Authorization='Bearer '+token),
+        data= json.dumps(self.data),
+        content_type='application/json')
+        
+        result = json.loads(response.data)
+        self.assertEqual(result['message'], 'Created successfully' )
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_all_products(self):
+        """Test if API can GET all products"""
+
+         #register user
+        response_reg = self.client.post('/api/v2/auth/signup', 
+        data= json.dumps(self.data_reg),
+        content_type='application/json')
+
+        #user login
+        response_login = self.client.post('/api/v2/auth/login', 
+        data= json.dumps(self.data_login),
+        content_type='application/json')
+        result_login = json.loads(response_login.data)
+        token = result_login['access_token']
+
+        #user post product
+        response = self.client.post('/api/v2/product',
+        headers = dict(Authorization='Bearer '+token),
+        data= json.dumps(self.data),
+        content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        
+        #user get all product
+        response= self.client.get('/api/v2/product',
+        headers = dict(Authorization='Bearer '+token))
+        result_get = json.loads(response.data)
+        self.assertEqual(result_get['message'], 'success')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_product_by_id(self):
+        """Test if API can GET single product by id"""
+
+         #register user
+        response_reg = self.client.post('/api/v2/auth/signup', 
+        data= json.dumps(self.data_reg),
+        content_type='application/json')
+
+        #user login
+        response_login = self.client.post('/api/v1/auth/login', 
+        data= json.dumps(self.data_login),
+        content_type='application/json')
+        result_login = json.loads(response_login.data)
+        token = result_login['access_token']
+
+        #user post product
+        response = self.client.post('/api/v2/product',
+        headers = dict(Authorization='Bearer '+token),
+        data= json.dumps(self.data),
+        content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        #user get one product
+        response = self.client.get('/api/v2/product/1',
+        headers = dict(Authorization='Bearer '+token))
+        result_get_one = json.loads(response.data)
+        self.assertEqual(result_get_one['message'], 'success')
+        self.assertEqual(response.status_code, 200)
+
     def tearDown(self):
         """Removes all initialised variables"""
         self.app_context.pop()
