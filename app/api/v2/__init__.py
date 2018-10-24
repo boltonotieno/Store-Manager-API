8af9d078777cc0aa9571ace0febb473086b23ec7
Bolton <bolton.otieno@gmail.com>
2018-10-24 19:31:56 +0300

    [Feature  #161454779] add url resource

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 from .views.registration_endpoint import Registration, User
+from .views.login_endpoint import Login
 
 
 version2 = Blueprint('api2', __name__, url_prefix='/api/v2')
 api = Api(version2)
 
 api.add_resource(Registration, '/auth/signup', '/users')
+api.add_resource(Login, '/auth/login')
 api.add_resource(User, '/users/<int:user_id>')
-
 
