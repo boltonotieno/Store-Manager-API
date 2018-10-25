fc1e770c41a12ca9cdb24a802500e9756ff8f801
Bolton <bolton.otieno@gmail.com>
2018-10-25 16:26:44 +0300

    [Chore #161478733] add database model

---
+++ app/api/v2/models/database_model.py
@@ -0,0 +1,39 @@
+
+class Database():
+    """Database class"""
+    def db_query(self):
+        query_users="""CREATE TABLE IF NOT EXISTS users(
+            userid SERIAL PRIMARY KEY UNIQUE NOT NULL,
+            name VARCHAR(50) NOT NULL,
+            username VARCHAR(50) NOT NULL UNIQUE,
+            email VARCHAR(150) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL,
+            gender VARCHAR(15) NOT NULL,
+            role VARCHAR(15) NOT NULL
+            )"""
+
+        query_category="""CREATE TABLE IF NOT EXISTS category(
+            categoryid SERIAL PRIMARY KEY UNIQUE NOT NULL,
+            name VARCHAR(50) UNIQUE NOT NULL
+            )"""
+
+        query_products="""CREATE TABLE IF NOT EXISTS products(
+            productid SERIAL PRIMARY KEY UNIQUE NOT NULL,
+            name VARCHAR(50) UNIQUE NOT NULL,
+            price INTEGER NOT NULL,
+            quantity INTEGER NOT NULL,
+            min_quantity INTEGER NOT NULL,
+            category VARCHAR(15) NOT NULL
+            )"""
+
+        query_sales="""CREATE TABLE IF NOT EXISTS sales(
+            saleid SERIAL PRIMARY KEY UNIQUE NOT NULL,
+            name VARCHAR(50) NOT NULL,
+            price INTEGER NOT NULL,
+            quantity INTEGER NOT NULL
+            )"""
+
+        self.query =[query_users,query_category,query_products,query_sales]
+        
+        return self.query
+
