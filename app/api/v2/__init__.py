afcc4464039ad737874e5eac4056402305573797
Bolton <bolton.otieno@gmail.com>
2018-10-25 04:43:54 +0300

    [Feature #161468827] add product resource

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, Blueprint
 from flask_restful import Api
 from .views.registration_endpoint import Registration, User
 from .views.login_endpoint import Login
+from .views.product_endpoint import Product
 
 
 version2 = Blueprint('api2', __name__, url_prefix='/api/v2')
@@ -10,4 +11,5 @@ api = Api(version2)
 api.add_resource(Registration, '/auth/signup', '/users')
 api.add_resource(Login, '/auth/login')
 api.add_resource(User, '/users/<int:user_id>')
+api.add_resource(Product, '/products')
 
