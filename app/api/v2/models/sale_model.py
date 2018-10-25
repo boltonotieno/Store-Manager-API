39790389d25de5ace77e25162a48c87d9cc3ae9f
Bolton <bolton.otieno@gmail.com>
2018-10-25 08:37:06 +0300

    [Chore #161470746] create sales model

---
+++ app/api/v2/models/sale_model.py
@@ -0,0 +1,63 @@
+import psycopg2
+import os
+from ..models import db_connection
+
+class Sales:
+    """Class contain sales model functions"""
+
+    connection = db_connection()
+    cursor = connection.cursor()
+
+    def create_table_sales(self):
+        """create Table sales"""
+
+        sql="""CREATE TABLE IF NOT EXISTS sales(
+        saleid SERIAL PRIMARY KEY UNIQUE NOT NULL,
+        name VARCHAR(50) NOT NULL,
+        price INTEGER NOT NULL,
+        quantity INTEGER NOT NULL
+        )"""
+        self.cursor.execute(sql)
+        self.connection.commit()
+
+    def drop_table_sales(self):
+        """drop Table sales"""
+
+        sql="""DROP TABLE sales"""
+        self.cursor.execute(sql)
+        self.connection.commit()
+
+    def create_sales(self):
+        """creates a new sale"""
+
+        sql="""INSERT INTO sales(name,price,quantity) VALUES(%s,%s,%s)"""
+
+        return sql
+
+    def get_all_sales(self):
+        """Fetch all sales"""
+
+        sql="SELECT * FROM sales"
+        
+        return sql
+
+    def get_one_sale(self):
+        """Fetch sale by id"""
+
+        sql="SELECT * FROM sales WHERE saleid = %s"
+        return sql
+
+    def modify_sales(self):
+        """modify a sales"""
+
+        sql="UPDATE sales SET name = %s, price = %s, quantity = %s WHERE saleid = %s"
+        
+        return sql
+
+    def delete_sales(self):
+        """delete single sale by id"""
+
+        sql="DELETE FROM sales WHERE saleid = %s"
+
+        return sql
+
