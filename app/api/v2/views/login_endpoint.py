3203d11bba2c9c8050c67528620285919e943b5a
Bolton <bolton.otieno@gmail.com>
2018-10-25 04:06:54 +0300

    [Feature #161454779] add login endpoint

--- app/api/v2/views/login_endpoint.py
+++ app/api/v2/views/login_endpoint.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
+import psycopg2
 from ..models.user_model import Users
-from ..models import user_model
+from ..models import db_connection
 
 from passlib.hash import sha256_crypt
 from flask import jsonify
@@ -12,4 +13,25 @@ parser.add_argument('password', help = 'This field cannot be blank', required = 
 
 class Login(Resource):
     def post(self):
-        pass
+
+        connection = db_connection()
+        cursor = connection.cursor()
+
+        data = parser.parse_args()
+        username = data['username']
+        password = data['password']
+
+        user = Users()
+        sql = user.get_user_by_username()
+        cursor.execute(sql,(username,))
+        data = cursor.fetchone()
+
+        if data is None:
+            return {'message' : 'User named {} not found'.format( username)}
+        
+        
+        # if sha256_crypt.verify(password, data[0][0]):
+
+        #     return {'message' : 'Logged in succesful'}
+
+        return {'message' : 'Logged in succesful'}
