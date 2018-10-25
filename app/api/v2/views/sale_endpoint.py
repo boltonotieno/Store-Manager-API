9aa2b63f818057c906a9744476cba55e9762c07e
Bolton <bolton.otieno@gmail.com>
2018-10-25 08:51:00 +0300

    [Feature #161471113] add POST method

--- app/api/v2/views/sale_endpoint.py
+++ app/api/v2/views/sale_endpoint.py
@@ -11,4 +11,26 @@ parser.add_argument('price', help = 'This field cannot be blank', required = Tru
 parser.add_argument('quantity', help = 'This field cannot be blank', required = True)
 
 class Sale(Resource):
-    pass
+
+   def post(self):
+        """Post new sales"""
+        connection = db_connection()
+        cursor = connection.cursor()
+
+        data = parser.parse_args()
+        name = data['name']
+        price = data['price']
+        quantity = data['quantity']
+        
+        try:
+            new_sales = Sales()
+            sql = new_sales.create_sales()
+            cursor.execute(sql,(name,price,quantity))
+            connection.commit()
+            
+            return {
+                    'message': 'Sales created successfully',
+                },201
+        except:
+            return {'message' : 'Sales Not Created'}
+
