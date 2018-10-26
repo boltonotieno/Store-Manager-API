bae2ee931527d18fa9995219cd36955c26b84ae0
Bolton <bolton.otieno@gmail.com>
2018-10-26 07:53:29 +0300

    [Chore #161483424] add db url

--- app/api/v2/models/__init__.py
+++ app/api/v2/models/__init__.py
@@ -8,7 +8,7 @@ def db_connection():
 
     url = "dbname='store_manager' host='localhost' port='5432' user='postgres'"
     # url = "dbname='store_manager' host='localhost' port='5432' user='bolt' password='root123!'"
-    url = os.getenv('DATABASE_URL')          
+    # url = os.getenv('DATABASE_URL')          
     connection = psycopg2.connect(url)
     
     return connection
