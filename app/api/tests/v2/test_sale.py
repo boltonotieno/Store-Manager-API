a5b19f114cfab1528a25d415acc69224b4ef1bd9
Bolton <bolton.otieno@gmail.com>
2018-10-29 19:28:59 +0300

    [Chore #161525972] refactor code

--- app/api/tests/v2/test_sale.py
+++ app/api/tests/v2/test_sale.py
@@ -97,67 +97,6 @@ class TestSales(unittest.TestCase):
         self.assertEqual(result['message'], {"quantity": "This field cannot be blank"})
         self.assertEqual(response.status_code, 400)
 
-
-    # def test_get_all_saless(self):
-    #     """Test if API can GET all saless"""
-
-    #     #register user
-    #     response_reg = self.client.post('/api/v2/auth/signup', 
-    #     data= json.dumps(self.data),
-    #     content_type='application/json')
-
-    #     #user login
-    #     response_login = self.client.post('/api/v2/auth/login', 
-    #     data= json.dumps(self.data_login),
-    #     content_type='application/json')
-    #     result_login = json.loads(response_login.data)
-    #     token = result_login['access_token']
-
-    #     #user post sales
-    #     response_post = self.client.post('/api/v2/sales',
-    #     headers = dict(Authorization='Bearer '+token),
-    #     data= json.dumps(self.data),
-    #     content_type='application/json')
-    #     self.assertEqual(response_post.status_code, 201)
-
-        
-    #     #user get all saless
-    #     response_get= self.client.get('/api/v2/sales',
-    #     headers = dict(Authorization='Bearer '+token))
-    #     result_get = json.loads(response_get.data)
-    #     self.assertEqual(result_get['message'], 'success')
-    #     self.assertEqual(response_get.status_code, 200)
-
-    # def test_get_sales_by_id(self):
-    #     """Test if API can GET single sales by id"""
-
-    #     #register user
-    #     response_reg = self.client.post('/api/v2/auth/signup', 
-    #     data= json.dumps(self.data),
-    #     content_type='application/json')
-
-    #     #user login
-    #     response_login = self.client.post('/api/v2/auth/login', 
-    #     data= json.dumps(self.data_login),
-    #     content_type='application/json')
-    #     result_login = json.loads(response_login.data)
-    #     token = result_login['access_token']
-
-    #     #user post sales
-    #     response = self.client.post('/api/v2/sales', 
-    #     headers = dict(Authorization='Bearer '+token),
-    #     data= json.dumps(self.data),
-    #     content_type='application/json')
-    #     self.assertEqual(response.status_code, 201)
-
-    #     #user get one sales
-    #     response = self.client.get('/api/v2/sales/1',
-    #     headers = dict(Authorization='Bearer '+token))
-    #     result_get_one = json.loads(response.data)
-    #     self.assertEqual(result_get_one['message'], 'success')
-    #     self.assertEqual(response.status_code, 200)
-
-
     def test_modify_sales(self):
         """Test if API can modify(PUT) a single sale"""
 
