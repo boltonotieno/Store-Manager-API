09316e22c9b245a337aa0b0303a102055640ca37
Bolton <bolton.otieno@gmail.com>
2018-11-07 20:14:03 +0300

    deploy

--- app/api/v2/models/__init__.py
+++ app/api/v2/models/__init__.py
@@ -9,14 +9,10 @@ def db_connection():
     # url = "dbname='store_manager' host='localhost' \
     #   port='5432' user='postgres'"
 
-    # url = "dbname='test_store_manager' host='localhost' port='5432' \
-    #  user='bolt'password='root123!'"
-    if env is 'testing':
-        url = "dbname='test_store_manager' host='localhost' port='5432' \
-                user='bolt'password='root123!'"
-    else:
-        url = os.getenv('DATABASE_URL')
-        
+    url = "dbname='test_store_manager' host='localhost' port='5432' \
+     user='bolt'password='root123!'"
+     
+    # url = os.getenv('DATABASE_URL')
     connection = psycopg2.connect(url)
     return connection
 
