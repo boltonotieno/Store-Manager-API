c12a5f16574b6ae6dc66579c0f9936be12e47f24
Bolton <bolton.otieno@gmail.com>
2018-11-09 12:23:18 +0300

    [Chore #161591812] add citext

--- app/api/v2/models/database_model.py
+++ app/api/v2/models/database_model.py
@@ -2,10 +2,12 @@
 class Database():
     """Database class"""
     def db_query(self):
+        query_citext = """CREATE EXTENSION IF NOT EXISTS citext"""
+
         query_users = """CREATE TABLE IF NOT EXISTS users(
             userid SERIAL PRIMARY KEY UNIQUE NOT NULL,
             name VARCHAR(50) NOT NULL,
-            username citext NOT NULL UNIQUE,
+            username CITEXT NOT NULL UNIQUE,
             email citext NOT NULL UNIQUE,
             password VARCHAR(255) NOT NULL,
             gender VARCHAR(15) NOT NULL,
@@ -14,12 +16,12 @@ class Database():
 
         query_category = """CREATE TABLE IF NOT EXISTS category(
             categoryid SERIAL PRIMARY KEY UNIQUE NOT NULL,
-            name citext UNIQUE NOT NULL
+            name CITEXT UNIQUE NOT NULL
             )"""
 
         query_products = """CREATE TABLE IF NOT EXISTS products(
             productid SERIAL PRIMARY KEY UNIQUE NOT NULL,
-            name citext UNIQUE NOT NULL,
+            name CITEXT UNIQUE NOT NULL,
             price INTEGER NOT NULL,
             quantity INTEGER NOT NULL,
             min_quantity INTEGER NOT NULL,
@@ -35,7 +37,8 @@ class Database():
             attendant VARCHAR(10) NOT NULL
             )"""
 
-        self.query = [query_users, query_category, query_products, query_sales] 
+        self.query = [query_citext, query_users, query_category,
+                      query_products, query_sales] 
         return self.query
 
     def drop_query(self):
